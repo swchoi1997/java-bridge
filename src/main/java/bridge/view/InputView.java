1b9b6f329568d459bbf5fb8b3f152416c0cce71b
sangwon <swchoi1997@naver.com>
2022-11-17 14:56:28 +0900

    feat: 출력 메세지 추가

--- src/main/java/bridge/view/InputView.java
+++ src/main/java/bridge/view/InputView.java
@@ -4,10 +4,15 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
 
+    private static final String BRIDGE_SIZE_MESSAGE = "다리의 길이를 입력해주세요.";
+    private static final String MOVING_MESSAGE = "이동할 칸을 선택해주세요. (위: U, 아래: D)";
+    private static final String GAME_COMMAND_MESSAGE = "게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)";
+
     /**
      * 다리의 길이를 입력받는다.
      */
     public String readBridgeSize() {
+        System.out.println(BRIDGE_SIZE_MESSAGE);
         return Console.readLine();
     }
 
@@ -15,6 +20,7 @@ public class InputView {
      * 사용자가 이동할 칸을 입력받는다.
      */
     public String readMoving() {
+        System.out.println(MOVING_MESSAGE);
         return Console.readLine();
     }
 
@@ -22,6 +28,7 @@ public class InputView {
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
     public String readGameCommand() {
+        System.out.println(GAME_COMMAND_MESSAGE);
         return Console.readLine();
     }
 
